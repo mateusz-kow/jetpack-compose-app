e280411341bd5b972aba08cbf42c3c2f0a02caae
mateusz-kow <kowalski.mateusz.1lo1@gmail.com>
2026-01-29 18:38:38 +0100

    Added more professional icons

--- app/src/main/java/com/example/jetpackcomposeapp/ui/navigation/NavigationItem.kt
+++ app/src/main/java/com/example/jetpackcomposeapp/ui/navigation/NavigationItem.kt
@@ -1,23 +1,31 @@
 package com.example.jetpackcomposeapp.ui.navigation
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.Cameraswitch
+import androidx.compose.material.icons.filled.Details
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Pets
+import androidx.compose.material.icons.filled.Photo
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class NavigationItem(val route: String, val title: String, val icon: ImageVector) {
     object Home : NavigationItem("home", "Dom", Icons.Default.Home)
-    object Cats : NavigationItem("list", "Koty", Icons.Default.Lock)
-    object Gallery : NavigationItem("gallery", "Galeria", Icons.Default.ShoppingCart)
-    object Detail : NavigationItem("detail/{catId}", "Detail", Icons.Default.Favorite)
-    object Edit : NavigationItem("edit/{catId}", "Edit", Icons.Default.Favorite)
-    object Add : NavigationItem("add", "Add Cat", Icons.Default.Call)
-    object Camera : NavigationItem("camera", "Camera", Icons.Default.Call)
-    object CameraWithCallback : NavigationItem("camera/{callbackMode}", "Camera", Icons.Default.Call)
-    object Viewer : NavigationItem("viewer/{catId}/{imageIndex}", "Viewer", Icons.Default.ShoppingCart)
-    object GalleryViewer : NavigationItem("galleryViewer/{imageIndex}", "Gallery Viewer", Icons.Default.ShoppingCart)
-    object GallerySelect : NavigationItem("gallerySelect/{callbackKey}", "Gallery Select", Icons.Default.ShoppingCart)
+    object Cats : NavigationItem("list", "Koty", Icons.Default.Pets)
+    object Gallery : NavigationItem("gallery", "Galeria", Icons.Default.Photo)
+    object Detail : NavigationItem("detail/{catId}", "Szczegóły", Icons.Default.Info)
+    object Edit : NavigationItem("edit/{catId}", "Edycja", Icons.Default.Edit)
+    object Add : NavigationItem("add", "Dodaj kota", Icons.Default.Add)
+    object Camera : NavigationItem("camera", "Kamera", Icons.Default.CameraAlt)
+    object CameraWithCallback : NavigationItem("camera/{callbackMode}", "Kamera", Icons.Default.Cameraswitch)
+    object Viewer : NavigationItem("viewer/{catId}/{imageIndex}", "Viewer", Icons.Default.Photo)
+    object GalleryViewer : NavigationItem("galleryViewer/{imageIndex}", "Gallery Viewer", Icons.Default.Photo)
+    object GallerySelect : NavigationItem("gallerySelect/{callbackKey}", "Gallery Select", Icons.Default.Photo)
 }
